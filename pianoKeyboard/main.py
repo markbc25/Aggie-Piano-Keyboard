0679e6e958e726a93ca5cf024c1df681d87a130c
Mark Costa <90577625+markbc25@users.noreply.github.com>
2022-03-01 22:31:36 -0600

    Add files via upload

---
+++ pianoKeyboard/main.py
@@ -0,0 +1,12 @@
+from playsound import playsound
+import winsound
+# playsound('C:/Users/Arnav/PycharmProjects/Pianokeyboard/piano-c_C_major.wav')
+# playsound('C:/Users/Arnav/PycharmProjects/Pianokeyboard/piano-g_G_major.wav')
+# playsound('C:/Users/Arnav/PycharmProjects/Pianokeyboard/piano-e_E_major.wav')
+# filename1 = 'C:/Users/Arnav/PycharmProjects/Pianokeyboard/piano-g_G_major.wav'
+# filename2 = 'C:/Users/Arnav/PycharmProjects/Pianokeyboard/piano-c_C_major.wav'
+# soundfile = filename1
+# winsound.PlaySound(soundfile, winsound.SND_FILENAME | winsound.SND_ASYNC)
+# playsound(filename2)
+# C:/Users/Arnav/PycharmProjects/Pianokeyboard/
+# C:/Users/Arnav/PycharmProjects/Pianokeyboard/
